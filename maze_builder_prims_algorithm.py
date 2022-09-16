0cc73c3df01c6c7f7ca8c0495cf3791d7ca5f329
Daniel Arturi <danielarturi@Daniels-MacBook-Pro.local>
2022-09-16 17:00:31 -0400

    Adjacent Cells Function
    The function added in this commit accepts the height and width parameters along with the coordinate points of a cell in list form as input. In return the function calculates all the adjacent cells that are not on the border of the maze and returns those cells to the caller in list form.

--- maze_builder_prims_algorithm.py
+++ maze_builder_prims_algorithm.py
@@ -13,10 +13,42 @@ def create_blank_maze(row_num, col_num):
 
 def select_random_start_cell(height, width):
     # Pick random index for start cell
-    y_coordinate = 1 + randrange(height - 2)
-    x_coordinate = 1 + randrange(width - 2)
+    x_coordinate = 1 + randrange(height - 2)
+    y_coordinate = 1 + randrange(width - 2)
 
     return [x_coordinate, y_coordinate]
+
+
+# Checks if the cell passed in as the third parameter is a border
+def check_if_border(height, width, cell):
+    x, y = cell[0], cell[1]
+    if x == 0 or x == height - 1 or y == 0 or y == width - 1:
+        # Returns true if the cell is in fact a border cell
+        return True
+    # Returns false if the cell is not a border cell
+    return False
+
+
+def get_adj_cells(height, width, cell):
+    x, y = cell[0], cell[1]
+
+    # Creates a list of all cells adjacent to the one passed in as input
+    adj_cell_list = []
+    for row in range(x-1, x+2):
+        for col in range(y-1, y+2):
+            adj_cell_list.append([row, col])
+
+    # Removes original cell
+    adj_cell_list.remove(cell)
+
+    # Remove all the cells that are adjacent to the original cell but are walls
+    non_border_cells = []
+    for i in adj_cell_list:
+        if not check_if_border(height, width, i):
+            non_border_cells.append(i)
+
+    # return the results of the calculations of the function
+    return non_border_cells
 
 
 def maze_creator():
@@ -33,12 +65,22 @@ def maze_creator():
 
     # start off the program with a single random cell
     start_cell = select_random_start_cell(height, width)
+    # Add cell to visited cells list
     visited_cells.append(start_cell)
+    # Edit graphical representation to reflect visitation of random start cell
+    maze[start_cell[0]][start_cell[1]] = ["v"]
+
+    for i in maze:
+        print(i)
+
+    print(start_cell)
+    print(get_adj_cells(height, width, start_cell))
 
 
 def main():
-    for i in create_blank_maze(5, 10):
-        print(i)
+    maze_creator()
+    # print(get_adj_cells(5, 10, [1, 1]))
+    # print(check_if_border(5, 10, [2, 2]))
 
 
 main()
