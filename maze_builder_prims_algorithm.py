385946ab41ae3a8e3173b3b2676ca91b44cce954
Daniel Arturi <danielarturi@Daniels-MacBook-Pro.local>
2022-09-16 10:20:10 -0400

    added start cell selection function
    added a function to select a non-border start cell. Also added another function to better organize the code without having to use global variables but it is a bit of a trivial addition

--- maze_builder_prims_algorithm.py
+++ maze_builder_prims_algorithm.py
@@ -1,3 +1,6 @@
+from random import randrange
+
+
 def create_blank_maze(row_num, col_num):
     return_array = []
     for row in range(row_num):
@@ -6,6 +9,31 @@ def create_blank_maze(row_num, col_num):
             row_list.append(['u'])
         return_array.append(row_list)
     return return_array
+
+
+def select_random_start_cell(height, width):
+    # Pick random index for start cell
+    y_coordinate = 1 + randrange(height - 2)
+    x_coordinate = 1 + randrange(width - 2)
+
+    return [x_coordinate, y_coordinate]
+
+
+def maze_creator():
+    # define height and width
+    height = 5
+    width = 10
+
+    # create blank maze
+    maze = create_blank_maze(height, width)
+
+    # initialize relevant lists
+    wall_list = []
+    visited_cells = []
+
+    # start off the program with a single random cell
+    start_cell = select_random_start_cell(height, width)
+    visited_cells.append(start_cell)
 
 
 def main():
---
+++ scratch_file.py
@@ -0,0 +1,4 @@
+from random import randrange
+
+for i in range(30):
+    print(randrange(10))
