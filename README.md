1155ad050cb8fd1d094979dcbd20b7d0a6a6f13c
Daniel Arturi <danielarturi@Daniels-MacBook-Pro.local>
2022-09-16 09:05:04 -0400

    Blank maze creator function implemented
    first commit of the project, all that this commit is really adding is a function that will create a nested list with all cells marked as unvisited. This commit also happens to include functionality to print out that array in a more visual manner, but that is a temporary solution.

---
+++ README.md
@@ -0,0 +1,4 @@
+This project is very much still a work in progress. 
+However, the plans are ambitious, yet hopefully attainable.
+For the first step of this project I will be using Prim's 
+Randomized Algorithm to create a randomized maze. 
---
+++ maze_builder_prims_algorithm.py
@@ -0,0 +1,16 @@
+def create_blank_maze(row_num, col_num):
+    return_array = []
+    for row in range(row_num):
+        row_list = []
+        for col in range(col_num):
+            row_list.append(['u'])
+        return_array.append(row_list)
+    return return_array
+
+
+def main():
+    for i in create_blank_maze(5, 10):
+        print(i)
+
+
+main()
